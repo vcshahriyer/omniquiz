5a262480015e66a79fe251515f12c2524dc5e1bd
vcshahriyer <shahriyer99@gmail.com>
2019-12-04 09:29:57 +0600

    Updates

---
+++ precache-manifest.b3ca97bcd4c163dcee918369ba0a3572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151d14177d65762cb57ef61f14f2b645",
+    "url": "/omniquiz/index.html"
+  },
+  {
+    "revision": "b01d864cfb0d4e823a71",
+    "url": "/omniquiz/static/css/2.6771fd38.chunk.css"
+  },
+  {
+    "revision": "8c155ce681d7b9769215",
+    "url": "/omniquiz/static/css/main.64273898.chunk.css"
+  },
+  {
+    "revision": "b01d864cfb0d4e823a71",
+    "url": "/omniquiz/static/js/2.0341ac37.chunk.js"
+  },
+  {
+    "revision": "8c155ce681d7b9769215",
+    "url": "/omniquiz/static/js/main.51067047.chunk.js"
+  },
+  {
+    "revision": "8588049e8aea4c81dff3",
+    "url": "/omniquiz/static/js/runtime-main.c7188865.js"
+  },
+  {
+    "revision": "2e052da910e7e2c225f2ca371ea837f1",
+    "url": "/omniquiz/static/media/party.2e052da9.png"
+  }
+]);
